fc26a5bd3ac52ffd7a89a9c17638683ab19edd9a
Leon Blakey <leon.m.blakey@gmail.com>
2015-06-09 20:37:41 -0400

    batch-commit-versions.sh: Commit all maven-metadata.xml files first

--- batch-commit-versions.sh
+++ batch-commit-versions.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+#First, commit all the metadata files
+cd maven
+find . -name '*.xml*' | xargs git add
+git commit -m "Added maven-metadata.xml files"
+
 #get all the version numbers, reasonably safe to assume this isn't disappearing
-cd maven/org/eclipse/swt/org.eclipse.swt.gtk.linux.x86_64
+cd org/eclipse/swt/org.eclipse.swt.gtk.linux.x86_64
 VERSIONS=$( ls -d */ | rev | cut -c 2- | rev | sort -t. -n -k1,1 -k2,2 -k3,3 -k4,4 )
 
 cd ..
