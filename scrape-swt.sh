b92a54548470efd3e1929c0e8a8c1675cab2cfda
Leon Blakey <leon.m.blakey@gmail.com>
2015-09-16 10:44:17 -0400

    scrape-swt: Split out release scraping into stage_1_scrape_releases

--- scrape-swt.sh
+++ scrape-swt.sh
@@ -34,17 +34,8 @@ stage_0_init() {
 	rm -rf tmp downloads
 }
 
-stage_1_scrape() {
-	mkdir -p tmp
-	cd tmp
-	# 1) Get HTML of root (follow redirects)
-	# 2) Ghetto match all urls in the common drops4 dir
-	# 3) Only care about releases and milestones
-	# 4) Filter out duplicates
-	curl -L $MIRROR > index.html
-	RELEASES=$( cat index.html | grep -E -o 'drops4/[a-zA-Z0-9\.-]+' | grep -E '^drops4/[SR]' | sort | uniq )
-
-	for CUR_RELEASE in $RELEASES;
+stage_1_scrape_releases() {
+	for CUR_RELEASE in "$@";
 	do
 		# for loop over above
 		# 1) Get HTML of specific release index (follow redirects)
@@ -52,7 +43,9 @@ stage_1_scrape() {
 		# 3) Cut off the ending "> (apparently 3 is needed for a hidden character)
 		#TODO: SWT specific
 		RELEASE_FILE=$( basename $CUR_RELEASE ).html
-		curl -L $MIRROR/$CUR_RELEASE > $RELEASE_FILE
+		URL=$MIRROR$CUR_RELEASE/
+		echo "Dumping $URL"
+		curl -L $URL > $RELEASE_FILE
 		DIST_FILES=$( cat $RELEASE_FILE | grep -E -o 'swt-[a-zA-Z0-9\._-]+\">' | grep -v "sha1\|md5\|sha512" | rev | cut -c 3- | rev | sort | uniq )
 
 		for CUR_FILE in $DIST_FILES;
@@ -62,6 +55,19 @@ stage_1_scrape() {
 			echo $MIRROR/$CUR_RELEASE/checksum/$CHECKSUM_FILE >> urls.txt
 		done;
 	done;
+}
+
+stage_1_scrape() {
+	mkdir -p tmp
+	cd tmp
+	# 1) Get HTML of root (follow redirects)
+	# 2) Ghetto match all urls in the common drops4 dir
+	# 3) Only care about releases and milestones
+	# 4) Filter out duplicates
+	curl -L $MIRROR > index.html
+	RELEASES=$( cat index.html | grep -E -o 'drops4/[a-zA-Z0-9\.-]+' | grep -E '^drops4/[SRM]' | sort | uniq )
+
+	stage_1_scrape_releases $RELEASES
 	cd ..
 }
 
